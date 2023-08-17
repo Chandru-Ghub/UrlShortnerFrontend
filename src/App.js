ade116fb96f5be7488e4d873f0981690c807f988
Chandru-Ghub <chandruinfo455@gmail.com>
2023-08-17 15:04:45 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3200/shorturl", {
+        url: url,
+      });
+
+      if (response.data.shortURL) {
+        setShortUrl(response.data.shortURL);
+      }
+    } catch (error) {
+      console.error("Error creating short URL:", error);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cont">
+     
+      <div className="App">
+      <h2>URL SHORTENER</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+          className="inp"
+            type="text"
+            placeholder="Paste your long URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button type="submit">Shorten</button>
+        </div>
+        <img src="https://www.sms-magic.com/wp-content/uploads/2021/01/Shorten-URL-1536x952.png" alt="image" />
+      </form>
+      {shortUrl && (
+        <div>
+          <div className="out">
+            
+              <span>Short URL: 👉</span>
+              <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+                {shortUrl}
+              </a>
+          </div>
+        </div>
+      )}
+    </div>
+       
     </div>
   );
 }
